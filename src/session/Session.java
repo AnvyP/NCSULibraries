a9bfaf0a72e86314f5d179640869a2666a1e08bf
Anvesh <apatlol2@ncsu.edu>
2015-10-12 16:28:36 -0400

    Session class

---
+++ src/session/Session.java
@@ -0,0 +1,50 @@
+package session;
+
+/**
+ * This class is intended to store all the properties of a session. Also, This class should be
+ * accessible by all other classes.
+ * 
+ * @author anvesh
+ * 
+ */
+public class Session {
+
+  private static Session sInstance = null;
+  private String username = null;
+
+  private String userId = null;
+  private String password = null;
+
+  /**
+   * This variable may only be used to implement time-outs in session. For all I know, this is not part of the
+   * specifications doc so far.
+   */
+  private int timeToLive = 0;
+
+  /**
+   * Useful for implementing session-timeouts.
+   */
+  private boolean sessionStarted = false;
+  
+  public boolean isSessionStarted() {
+    return sessionStarted;
+  }
+
+  public void setSessionStarted(boolean sessionStarted) {
+    this.sessionStarted = sessionStarted;
+  }
+
+  private Session() {
+
+  }
+
+  public static synchronized Session getInstance() {
+    if (sInstance == null) {
+      // TODO: change the instance
+      sInstance = new Session();
+    }
+    return sInstance;
+  }
+  
+  
+}
