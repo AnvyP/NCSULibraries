8f34ecbb8d7fd6f5a93f130eae467084e553e262
Anvesh <apatlol2@ncsu.edu>
2015-10-20 18:05:56 -0400

    PHP login

---
+++ src/php/login/StudentLogin.php
@@ -0,0 +1,12 @@
+<?php
+echo "<form action=\"CheckCredentials.php\">
+  UNITY ID:<br>
+  <input type=\"text\" name=\"Name\" value=\" ENTER NAME HERE\">
+  <br>
+  PASSWORD:<br>
+  <input type=\"password\" name=\"PASSWORD\" value=\"\">
+  <br><br>
+  <input type=\"submit\" value=\"Submit\">
+</form>";
+
+?>
---
+++ src/php/login/index.php
@@ -0,0 +1,33 @@
+<?php
+echo "<HTML>";
+echo "<BODY>";
+
+echo "select one of the following";
+
+echo "<TABLE border width = \"1\" style=\"width = 100%\">";
+
+echo "<tr>";
+echo "<td>";
+echo "aa";
+echo "<a href=\"StudentLogin.php\">STUDENT LOGIN </a>";
+//echo "bb";
+echo "<\td>";
+echo "\tr";
+
+echo "<tr>";
+echo "<td>";
+echo "<a href=\"FacultyLogin.php\">FACULTY LOGIN </a>";
+echo "<\td>";
+
+echo "\tr";
+
+echo "<>";
+
+
+echo "</BODY>";
+
+
+
+echo "</HTML>";
+
+?>
