bc1401c3b6e1647efff6b02024a30ffe94a2bc32
Anvesh <apatlol2@ncsu.edu>
2015-10-21 19:58:49 -0400

    Added Faculty and Student Login pages.

--- src/php/login/CheckCredentials.php
+++ src/php/login/CheckCredentials.php
@@ -14,18 +14,25 @@ echo "TYPE OF USER:".$_REQUEST["user"];
 
 echo "<br> <br>";
 
-// if credentials are okay then.
 
-// if credentials fails =>
-//header('Location: Login.php');
+require_once ('../connections/VerifyCredentials.php');
 
-session_start();
-$_SESSION['NAME'] = $_REQUEST["Name"];
-$_SESSION['USER'] = $_REQUEST["user"];
 
-if($_SESSION['USER'] == "STUDENT"){
-	header('Location: student/HomePage.php');
+if(verifyCredentials($conn,$_REQUEST["Name"],$_REQUEST["PASSWORD"])){
+	// if credentials are okay then.
+	session_start();
+	$_SESSION['NAME'] = $_REQUEST["Name"];
+	$_SESSION['USER'] = $_REQUEST["user"];
+
+	if($_SESSION['USER'] == "STUDENT"){
+		header('Location: student/HomePage.php');
+	}else{
+		header('Location: faculty/HomePage.php');
+	}
 }else{
-	header('Location: faculty/HomePage.php');
+	// if credentials fails =>
+	//header('Location: Login.php');
+	header('Location: index.php');
 }
+
 ?>
