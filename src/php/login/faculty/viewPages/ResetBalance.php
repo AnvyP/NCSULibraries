1d763c96cfbafdade857a5550829437d9427b17f
Anvesh <apatlol2@ncsu.edu>
2015-11-01 17:37:31 -0500

    Faculty -Reset Balance

--- src/php/login/faculty/viewPages/ResetBalance.php
+++ src/php/login/faculty/viewPages/ResetBalance.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 if(!isset($_SESSION['NAME'])){
@@ -9,21 +10,14 @@ $conn = null;
 require_once('../../../connections/Connection.php');
 $UnityId = $_SESSION['NAME'];
 
-$query = "select \"Balance\" from LIBRARYPATRON where \"UnityId\"='".$UnityId."'";
+$query = "update LIBRARYPATRON set \"Balance\"=0 where \"UnityId\"='".$UnityId."'";
 var_dump($query);
 $stid = oci_parse($conn, $query);
 $result = oci_execute($stid);
 if (!$result) {
 	echo oci_error();
 }else{
-	//working fine.
+	header( "Location: AccountBalance.php" );
 }
-$row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS);
-$balance =$row['Balance'];
-echo "Your balance ".$balance;
-if($balance < 0){
-	echo "click here to clear your account Balance <a href=\"ResetBalance.php\">Clear Balance</a>";
-}
-
 
 ?>
