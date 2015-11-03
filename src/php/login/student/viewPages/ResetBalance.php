ea5c4c33458512945bec52e6ee86359227072c12
Ashwin Bharadwaj <alakshm6@ncsu.edu>
2015-11-02 23:19:25 -0500

    Inserted transaction to account history

--- src/php/login/student/viewPages/ResetBalance.php
+++ src/php/login/student/viewPages/ResetBalance.php
@@ -10,6 +10,25 @@ $conn = null;
 require_once('../../../connections/Connection.php');
 $UnityId = $_SESSION['NAME'];
 
+$query = "SELECT \"Balance\" AS BALANCE FROM LIBRARYPATRON \"UnityId\"='".$UnityId."'";
+var_dump($query);
+$stid = oci_parse($conn, $query);
+$result = oci_execute($stid);
+$nrows = oci_fetch_all($stid, $balance, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+if($nrows == 1) {
+	$balanceAmount = $balance[0]['BALANCE'];
+	$query = "INSERT INTO ACCOUNT_HISTORY VALUES('{$UnityId}',SYSTIMESTAMP,{$balanceAmount},1)";
+	$stid = oci_parse($conn, $query);
+	$result = oci_execute($stid);
+	if(!$result) {
+		echo "Error while updating transaction table";
+	} else {
+		echo "Updated transaction successfully";
+	}
+} else {
+	echo "Some error occurred";
+}
+
 $query = "update LIBRARYPATRON set \"Balance\"=0 where \"UnityId\"='".$UnityId."'";
 var_dump($query);
 $stid = oci_parse($conn, $query);
@@ -17,6 +36,7 @@ $result = oci_execute($stid);
 if (!$result) {
 	echo oci_error();
 }else{
+	
 	header( "Location: AccountBalance.php" );
 }
 
