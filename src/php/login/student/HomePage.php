fe35f7dca7713b71aac551d8a3b059fc3db7fc0e
Anvesh <apatlol2@ncsu.edu>
2015-11-03 15:16:21 -0500

    Added check constraints for student- Degree Program vs Classification

--- src/php/login/student/HomePage.php
+++ src/php/login/student/HomePage.php
@@ -17,9 +17,9 @@ require_once('update\UpdateStudentInfoUtils.php');
 $UnityId = $_SESSION['NAME'] ;
 $query = "select S.\"UnityId\",S.\"StudentNo\",S.\"PhoneNo\",S.\"AlternatePhoneNo\",
 		S.\"HomeAddress\",S.\"DateOfBirth\",S.\"Sex\",S.\"DegreeProgram\",S.\"Category\",
-		S.\"isHeld\",L.\"Name\",L.\"Nationality\",L.\"Department\",L.\"Type\",L.\"Balance\" 
-		FROM STUDENT S, LIBRARYPATRON L WHERE S.\"UnityId\" = L.\"UnityId\" 
-		and S.\"UnityId\" = "."'".$UnityId."'";
+		S.\"isHeld\",L.\"Name\",L.\"Nationality\",L.\"Department\",L.\"Type\",L.\"Balance\",DC.\"Classification\" 
+		FROM STUDENT S, LIBRARYPATRON L,DEGREE_CLASSIFICATION DC WHERE S.\"UnityId\" = L.\"UnityId\" 
+		and S.\"DegreeProgram\"=DC.\"Degree\" AND S.\"UnityId\" = "."'".$UnityId."'";
 $nextPage = "update/UpdateStudentInfo.php";
 fetchAndUpdateStudentInfo($query,$conn,$nextPage);
 
--- src/php/login/student/update/UpdateStudentInfo.php
+++ src/php/login/student/update/UpdateStudentInfo.php
@@ -15,18 +15,40 @@ $DateOfBirth = $_REQUEST['DateOfBirth'];
 $Sex = $_REQUEST['Sex'];
 $Nationality = $_REQUEST['Nationality'];
 //$Department = $_REQUEST['Department'];
-//$Classification = $_REQUEST['Classification'];
-//$DegreeProgram = $_REQUEST['DegreeProgram'];
+$Classification = $_REQUEST['Classification'];
+$DegreeProgram = $_REQUEST['DegreeProgram'];
 //$Category = $_REQUEST['Category'];
 
 $UnityId = $_SESSION['NAME'];
+
+
+$DegreeClassificationQuery = "SELECT * from DEGREE_CLASSIFICATION
+WHERE \"Degree\"='$DegreeProgram' AND \"Classification\"='$Classification'";
+
+var_dump($DegreeClassificationQuery);
+
+$stid = oci_parse($conn, $DegreeClassificationQuery);
+$result = oci_execute($stid);
+//exit(0);
+if (!$result) {
+	echo oci_error();
+}else{
+	//	header('Location: ../HomePage.php');
+}
+
+$nrows = oci_fetch_all($stid, $isHeldResult, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+if($nrows == 0) {
+	echo "Invalid degree program corresponding to classification has been added. Please check the appropriate ones.";
+	echo "<br><br> Click <a href=\"../HomePage.php\"> here to get redirected to home page</a>";
+	exit(0);
+}
 
 
 $query = "UPDATE STUDENT
 SET \"PhoneNo\"='$PhoneNo',
 \"AlternatePhoneNo\"='$AlternatePhoneNo',\"HomeAddress\"='$HomeAddress',
 \"DateOfBirth\"='$DateOfBirth',\"Sex\"='$Sex'
- WHERE \"UnityId\"='$UnityId'";
+WHERE \"UnityId\"='$UnityId'";
 var_dump($query);
 $stid = oci_parse($conn, $query);
 $result = oci_execute($stid);
@@ -34,12 +56,12 @@ $result = oci_execute($stid);
 if (!$result) {
 	echo oci_error();
 }else{
-//	header('Location: ../HomePage.php');
+	//	header('Location: ../HomePage.php');
 }
 
-$query = "UPDATE LIBRARYPATRON 
-		SET \"Name\"='$Name',\"Nationality\"='$Nationality' 
-		WHERE \"UnityId\"='$UnityId'		";
+$query = "UPDATE LIBRARYPATRON
+SET \"Name\"='$Name',\"Nationality\"='$Nationality'
+WHERE \"UnityId\"='$UnityId'		";
 var_dump($query);
 $stid = oci_parse($conn, $query);
 $result = oci_execute($stid);
