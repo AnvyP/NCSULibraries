33fb15c89ee0bd76d56ec9fe38e71359e2ee59c8
Ashwin Bharadwaj <alakshm6@ncsu.edu>
2015-11-03 15:11:54 -0500

    Modified insert notifications query

--- src/php/login/faculty/viewPages/ReserveBookForCourseUtil.php
+++ src/php/login/faculty/viewPages/ReserveBookForCourseUtil.php
@@ -28,7 +28,7 @@ if(!$result) {
 	echo "<br> <br>";
 }
 else {
-	$query = "INSERT INTO NOTIFICATION('{$UnityId}',SYSTIMESTAMP,'Book with ISBN' || $isbn || ' has been reserved for course ' || $course || ' and expires on ' || (SYSTIMESTAMP + INTERVAL '4' MONTH))";	
+	$query = "INSERT INTO NOTIFICATION VALUES('{$UnityId}',SYSTIMESTAMP,'Book with ISBN' || $isbn || ' has been reserved for course ' || $course || ' and expires on ' || (SYSTIMESTAMP + INTERVAL '4' MONTH))";	
 	$stid = oci_parse($conn,$query);
 	$result = oci_execute($stid);
 	if(!$result) {
